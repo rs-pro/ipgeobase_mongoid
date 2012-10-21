75d3420affddbcd02d29209ad8aa58a44b63fcfc
Roman Shterenzon <roman.shterenzon@gmail.com>
2012-10-21 02:17:07 +0200

    Cleanup

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -3,35 +3,25 @@
 require "ipgeobase/version"
 
 module Ipgeobase
-  if defined?(Rails) && defined?(Rails::Engine)
+  if defined?(Rails::Engine)
     class Engine < ::Rails::Engine
-    end
-
-    module Rails
-      class Railtie < ::Rails::Railtie
-      end
     end
   end
 
   def self.find_region_by_ip(user_ip)
-    raise ArgumentError, "Must have a ip addres by template 0.0.0.0" unless user_ip =~ /^[\d\.]+$/
+    raise ArgumentError, "IP address is expected" unless user_ip =~ /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\Z/
     long = ip2long(user_ip)
     ip = IpgeobaseIp.where(start_ip: { '$lte' => long }).order_by({ start_ip: -1 }).limit(1).first
-    p ip
-    if ip.nil?
+    if ip.nil? or ip.end_ip < long
       nil
     else
-      if ip.end_ip < long
-        nil
-      else
-        ip.city
-      end
+      ip.city
     end
   end
 
   def self.ip2long(ip)
     long = 0
-    ip.split(/\./).reverse.each_with_index do |b, i|
+    ip.split('.').reverse.each_with_index do |b, i|
       long += b.to_i << (8*i)
     end
     long
