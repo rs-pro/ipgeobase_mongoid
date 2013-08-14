65c27a4f7875ee80fb37eb7eb53636b5cfb0f185
glebtv <glebtv@gmail.com>
2013-08-14 23:49:53 +0400

    failure specs

--- spec/ipgeobase_spec.rb
+++ spec/ipgeobase_spec.rb
@@ -8,4 +8,12 @@ describe Ipgeobase do
     geo.region.should eq 'Красноярский край'
     geo.country.code.should eq 'RU'
   end
+  it "doesn't return first or last city on fail" do
+    geo = Ipgeobase::find_region_by_ip('0.0.0.0')
+    geo.should be_nil
+    geo = Ipgeobase::find_region_by_ip('10.0.0.0')
+    geo.should be_nil
+    geo = Ipgeobase::find_region_by_ip('255.255.255.255')
+    geo.should be_nil
+  end
 end
